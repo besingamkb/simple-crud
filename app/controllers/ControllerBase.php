08961c4688bfb323e13515862cb2f14af8d2a786
Mark Kevin Besinga <besingamkb@github.com>
2014-05-23 06:45:02 +0800

    add redirect function

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -5,5 +5,8 @@ use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
+	public function to($url) {
+    	echo '<meta http-equiv="refresh" content="0;url=' . $this->url->getBaseUri() . $url .'" />';
+    }
 
 }
